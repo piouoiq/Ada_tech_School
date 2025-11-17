f7ef1d241ecea95671a8f4cad6f7aaf2aadda406
piouoiq <melargilliere@gmail.com>
2025-11-17 09:58:05 +0100

    ex2 clean code

---
+++ [GHN] [C12] Clean-Code/Exercice2/script.js
@@ -0,0 +1,80 @@
+// ________________________________
+//|Avant refactorisation
+//|________________________________
+
+// function genererRapportUtilisateur(nom, age, ville, profession, estAbonne) {
+//   let rapport = "Rapport utilisateur :\\n";
+//   rapport += "Nom : " + nom + "\\n";
+//   rapport += "Age : " + age + " ans\\n";
+//   rapport += "Ville : " + ville + "\\n";
+//   rapport += "Profession : " + profession + "\\n";
+//   if (estAbonne == true) {
+//     rapport += "Statut : Abonné\\n";
+//   } else {
+//     rapport += "Statut : Non abonné\\n";
+//   }
+//   if (age < 18) {
+//     rapport += "Catégorie : Jeune\\n";
+//   } else if (age >= 18 && age < 60) {
+//     rapport += "Catégorie : Adulte\\n";
+//   } else {
+//     rapport += "Catégorie : Senior\\n";
+//   }
+//   return rapport;
+// }
+
+// ________________________________
+//|Après refactorisation
+//|________________________________
+
+// function genererRapportUtilisateur(nom, age, ville, profession, estAbonne) {
+//   let rapport = "Rapport utilisateur :\\n";
+//   rapport += `Nom : ${nom} \\n`;
+//   rapport += `"Age :  ${age}  " ans\\n"`;
+//   rapport += `"Ville : "  ${ville}  "\\n"`;
+//   rapport += `"Profession : "  ${profession}  "\\n"`;
+//   if (estAbonne == true) {
+//     rapport += `"Statut : Abonné\\n"`;
+//   } else {
+//     rapport += `"Statut : Non abonné\\n"`;
+//   }
+
+//   return rapport;
+// }
+
+// function subscriberCategory() {
+//   if (subcriber.age < 18) {
+//     if (age < 18) {
+//       rapport += `"Catégorie : Jeune\\n"`;
+//     } else if (age >= 18 && age < 60) {
+//       rapport += `"Catégorie : Adulte\\n"`;
+//     } else {
+//       rapport += `"Catégorie : Senior\\n"`;
+//     }
+//   }
+//   return subscriberCategory();
+// }
+
+const statutAbonnement = (estAbonne) => (estAbonne ? "Abonné" : "Non abonné");
+
+const categorieAge = (age) => {
+  if (age < 18) return "Jeune";
+  if (age < 60) return "Adulte";
+  return "Senior";
+};
+
+const genererRapportUtilisateur = (nom, age, ville, profession, estAbonne) => `
+Rapport utilisateur :
+Nom : ${nom}
+Âge : ${age} ans
+Ville : ${ville}
+Profession : ${profession}
+Statut : ${statutAbonnement(estAbonne)}
+Catégorie : ${categorieAge(age)}
+`;
+
+console.log(genererRapportUtilisateur("Ron", 37, "London", "Wizzard", true));
+
+console.log(
+  genererRapportUtilisateur("Edward", 34, "Ipswich", "Singer", false)
+);
