4b2b742f3bdb59198402b00bbc0f0e24ab0fdad9
piouoiq <melargilliere@gmail.com>
2025-11-17 16:00:15 +0100

    fix ex2 clean code

--- [GHN] [C12] Clean-Code/Exercice2/script.js
+++ [GHN] [C12] Clean-Code/Exercice2/script.js
@@ -27,34 +27,6 @@
 //|Après refactorisation
 //|________________________________
 
-// function genererRapportUtilisateur(nom, age, ville, profession, estAbonne) {
-//   let rapport = "Rapport utilisateur :\\n";
-//   rapport += `Nom : ${nom} \\n`;
-//   rapport += `"Age :  ${age}  " ans\\n"`;
-//   rapport += `"Ville : "  ${ville}  "\\n"`;
-//   rapport += `"Profession : "  ${profession}  "\\n"`;
-//   if (estAbonne == true) {
-//     rapport += `"Statut : Abonné\\n"`;
-//   } else {
-//     rapport += `"Statut : Non abonné\\n"`;
-//   }
-
-//   return rapport;
-// }
-
-// function subscriberCategory() {
-//   if (subcriber.age < 18) {
-//     if (age < 18) {
-//       rapport += `"Catégorie : Jeune\\n"`;
-//     } else if (age >= 18 && age < 60) {
-//       rapport += `"Catégorie : Adulte\\n"`;
-//     } else {
-//       rapport += `"Catégorie : Senior\\n"`;
-//     }
-//   }
-//   return subscriberCategory();
-// }
-
 const statutAbonnement = (estAbonne) => (estAbonne ? "Abonné" : "Non abonné");
 
 const categorieAge = (age) => {
