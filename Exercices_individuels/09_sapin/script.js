daa726903caf08c27c87a08cc8859c06fa0fda2a
piouoiq <melargilliere@gmail.com>
2026-01-26 12:31:58 +0100

    step 1.5&2.1

--- Exercices_individuels/09_sapin/script.js
+++ Exercices_individuels/09_sapin/script.js
@@ -1,14 +1,14 @@
-//etape 1
-// let trunk = "|";
+// etape 1
+let trunk = "|";
 
-// function afficherPointeSapin() {
-//   let pointe = `+\n `;
+function afficherPointeSapin() {
+  let pointe = `+\n `;
 
-//   return pointe;
-// }
+  return pointe;
+}
 
-// document.getElementById("pointe").innerText =
-//   " etape 1 : \n" + afficherPointeSapin();
+document.getElementById("pointe").innerText =
+  " etape 1 : \n" + afficherPointeSapin();
 
 //etape 1.1
 function afficherEtoiles(n) {
@@ -67,39 +67,63 @@ function afficherTriangleGauche(n) {
 document.getElementById("triangleGauche").innerText =
   "etape 1.4 : \n" + afficherTriangleGauche(5);
 
-//etape 1.5
+// //etape 1.5
 
-function afficherSapin(etages, hauteur_etage) {
-  let sapin = afficherPointeSapin();
+// function afficherSapin(etages, hauteur_etage) {
+//   let sapin = afficherPointeSapin();
 
-  for (let i = 0; i < hauteur_etage; i++) {
-    let row = afficherEtoiles(i) + trunk + afficherEtoiles(i) + "\n";
-    for (let j = 0; j < hauteur_etage - i - 1; j++) {
-      row += " ";
-    }
-    sapin += row;
-  }
+//   for (let i = 0; i < hauteur_etage; i++) {
+//     let row = afficherEtoiles(i) + trunk + afficherEtoiles(i) + "\n";
+//     for (let j = 0; j < hauteur_etage - i - 1; j++) {
+//       row += " ";
+//     }
+//     sapin += row;
+//   }
 
-  return sapin;
-}
+//   return sapin;
+// }
 
-document.getElementById("sapin").innerText =
-  "etape 1.5 : \n" + afficherSapin(5, 5);
+// document.getElementById("sapin").innerText =
+//   "etape 1.5 : \n" + afficherSapin(5, 5);
 
-//etape 1.5 corrigé
+// //etape 1.5 corrigé
 
-function afficherPointeSapin(hauteur) {
+// function afficherPointeSapin(hauteur) {
+//   let sapin = "";
+//   sapin += " ".repeat(hauteur) + "+\n";
+
+//   for (let i = 0; i < hauteur; i++) {
+//     let espace = " ".repeat(hauteur - i - 1);
+//     let etoiles = afficherEtoiles(i);
+//     sapin += espace + etoiles + "\n";
+//   }
+
+//   return sapin;
+// }
+
+// document.getElementById("pointe").innerText =
+//   " etape 1 : \n" + afficherPointeSapin(5);
+
+///////////////////////////
+//////Etape 1.5 & 2.1//////
+///////////////////////////
+
+function afficherEtageSapin(height, pointe_offSet) {
   let sapin = "";
-  sapin += " ".repeat(hauteur) + "+\n";
 
-  for (let i = 0; i < hauteur; i++) {
-    let espace = " ".repeat(hauteur - i - 1);
-    let etoiles = afficherEtoiles(i);
-    sapin += espace + etoiles + "\n";
+  let trueHeight = height + pointe_offSet;
+  if (pointe_offSet < 0) {
+    console.error("L'offset ne peut pas etre négatif");
+    return;
+  }
+
+  for (let i = pointe_offSet; i < trueHeight; i++) {
+    let space = " ".repeat(trueHeight - i);
+    let etoiles = "*".repeat(i);
+    sapin += space + "/" + etoiles + "|" + etoiles + "\\\n";
   }
 
   return sapin;
 }
-
-document.getElementById("pointe").innerText =
-  " etape 1 : \n" + afficherPointeSapin(5);
+document.getElementById("sapin").innerText =
+  " etape 1.5 & 2.1 : \n" + afficherEtageSapin(3, 0) + afficherEtageSapin(3, 1);
